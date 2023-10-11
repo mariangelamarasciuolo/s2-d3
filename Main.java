87bf8d5bc2e73cb9d8b0b74c57af683dac3585f0
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-10-11 18:40:53 +0200

    esercizio 2
    finito

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Product prod1 = new Product(2023889L, "shadowHunters", "Books", 150.33);
         Product prod2 = new Product(2023889L, "Geronimo", "Books", 111.23);
-        Product prod3 = new Product(7125874L, "Biberon", "Baby", 6.5);
+        Product prod3 = new Product(7125874L, "Bieberon", "Baby", 6.5);
         Product prod4 = new Product(7125874L, "Pannolini", "Baby", 5.9);
         Product prod5 = new Product(5896423L, "VideoGames", "Boys", 20.8);
         Product prod6 = new Product(5896423L, "Shoes", "Boys", 30.6);
@@ -19,10 +19,10 @@ public class Main {
         pr1.add(prod1);
         pr1.add(prod2);
         pr1.add(prod3);
+        pr1.add(prod4);
 
 
         List<Product> pr2 = new ArrayList<>();
-        pr2.add(prod4);
         pr2.add(prod5);
         pr2.add(prod6);
 
@@ -33,6 +33,17 @@ public class Main {
 
         for (Product prod : piuDiCento) {
             System.out.println("Prodotti superiori a 100€:" + prod.toString());
+
+            /////////////////////ESERCIZIO 2
+
+            List<Order> prBaby = new ArrayList<>();
+            prBaby.add(ord1);
+            prBaby.add(ord2);
+            List<Order> categoryBaby = prBaby.stream().filter(x -> x.products.stream().anyMatch(product -> product.category.equals("Baby"))).toList();
+            for (Order ord : categoryBaby) {
+                System.out.println("Prodotti che appartengono alla lista baby: " + ord);
+            }
+
         }
     }
 }
